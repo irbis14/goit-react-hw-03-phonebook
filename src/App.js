3df0ed30c1c1bc37dad80782e194357dc4db4ed7
Nina Motorna <nina.motorna@gmail.com>
2021-06-25 19:46:38 +0200

    Saves contacts to localStorage

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
@@ -63,6 +58,19 @@ class App extends Component {
       contacts: restContacts,
     });
   };
+
+  componentDidMount() {
+    const initialContacts = JSON.parse(localStorage.getItem("contact"));
+    if (initialContacts) {
+      this.setState({ contacts: initialContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contact", JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
